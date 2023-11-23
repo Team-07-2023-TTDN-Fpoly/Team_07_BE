ca924a005936a297234d827fc0178ac681fc05a6
NghiaTC <nghiatcpd05529@fpt.edu.vn>
2023-11-23 09:46:32 +0700

    NghiaTC - Đổi getAll thành tìm kiếm theo tên

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,18 +1,16 @@
 const Authentication = require("../models/AuthenticationSchema.js");
 const bcrypt = require("bcryptjs");
 
-class AuthenticationController{
+class AuthenticationController {
   // Hàm cho người quản lý thay đổi mật khẩu nhân viên
   static async adminChangePassword(req, res) {
+    const { id } = req.params;
+    const { newPassword } = req.body;
     try {
-      const { id } = req.params;
-      const { newPassword } = req.body;
-
       const auth = await Authentication.findById(id);
       if (!auth) {
         return res.status(404).json({ message: "Người dùng không tồn tại." });
       }
-
       // Tạo hash mới cho mật khẩu mới
       const salt = bcrypt.genSaltSync(10);
       const hash_password = bcrypt.hashSync(newPassword, salt);
@@ -20,17 +18,17 @@ class AuthenticationController{
       auth.hash_password = hash_password;
       auth.salt = salt;
       await auth.save();
-
       res
         .status(200)
         .json({ message: "Mật khẩu đã được thay đổi thành công." });
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      console.log(error);
+      res.status(400).json({ message: error.message });
     }
   }
 
   // Hàm disable tài khoản
-  static async disableAccount(req, res){
+  static async disableAccount(req, res) {
     try {
       const { id } = req.params;
       const { disable } = req.body; // Giá trị boolean để biết là disable hay enable tài khoản
@@ -43,14 +41,13 @@ class AuthenticationController{
       }
 
       res.status(200).json({
-        message: `Tài khoản đã được ${
-          disable ? "vô hiệu hóa" : "kích hoạt"
-        } thành công.`,
+        message: `Tài khoản đã thay đổi thành công.`,
       });
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      console.log(error);
+      res.status(400).json({ message: error.message });
     }
   }
-};
+}
 
 module.exports = AuthenticationController;
