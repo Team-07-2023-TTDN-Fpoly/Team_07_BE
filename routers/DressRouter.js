7350863c2f96a8fd3226c00ff949fcb2a59acc6a
NghiaTC <nghiatcpd05529@fpt.edu.vn>
2023-12-01 22:29:23 +0700

    Change format contract and add AdminMiddleware

--- routers/DressRouter.js
+++ routers/DressRouter.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 const DressController = require("../controllers/DressController");
 const parser = require("../middleware/multer-config.js"); // The configured multer instance
+const AdminMiddleware = require("../middleware/AdminMiddleware.js");
 
 // Routes
 router.get("/:id", DressController.getDress);
 router.get("/", DressController.getAllDresses);
 router.post("/", parser.single("image"), DressController.addDress); // 'dress_image' is the name of the field in the form
 router.put("/:id", parser.single("image"), DressController.updateDress);
-router.delete("/:id", DressController.deleteDress);
+router.delete("/:id",AdminMiddleware, DressController.deleteDress);
 
 module.exports = router;
